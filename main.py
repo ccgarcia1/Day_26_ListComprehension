06ce9af2bb24b7a9f8c7d4a786e042455c62ad21
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2023-12-12 00:22:34 -0300

    Exercises List Comprehension

--- main.py
+++ main.py
@@ -25,3 +25,47 @@ print(result)
 names = ["Angela", "Beth", "Deise", "Erica", "Daniella"]
 result = [name.upper() for name in names if len(name) < 6]
 print(result)
+
+#Exercise 1
+#You are going to write a List Comprehension to create a new
+# list called squared_numbers. This new list should
+# contain every number in the list numbers but each number should be squared.
+
+#e.g. 4 * 4 = 16
+#4 squared equals 16.
+#DO NOT modify the List numbers directly. Try to use List Comprehension instead of a Loop.
+#Target Output
+#[1, 1, 4, 9, 25, 64, 169, 441, 1156, 3025]
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above 👆
+# Write your 1 line code 👇 below:
+squared_numbers = [square * square for square in numbers]
+# Write your code 👆 above: print(squared_numbers)
+
+
+#Exercise 2
+# In this list comprehension exercise you will practice using list comprehension to filter out the even numbers from a series of numbers.
+# First, use list comprehension to convert the list_of_strings to a list of integers.
+# Then use list comprehension again to create a new list called result. This new list should only contain the even numbers from the list numbers.
+# Again, try to use Python's List Comprehension instead of a Loop.
+list_of_strings = input().split(',')
+# 🚨 Do not change the code above
+# TODO: Use list comprehension to convert the strings to integers 👇:
+list_of_integers = [eval(number) for number in list_of_strings]
+# TODO: Use list comprehension to filter out the odd numbers
+# and store the even numbers in a list called "result"
+result = [num for num in list_of_integers if num%2==0]
+# Write your code 👆 above:
+print(result)
+
+
+#Exercise 3
+#Finding the duplicate values between 2 files .txt using list comprehension:
+with open("file1.txt", mode="r") as file:
+    list1 = file.readlines()
+with open("file2.txt", mode="r") as file:
+    list2 = file.readlines()
+result = [int(num) for num in list1 if num in list2]
+# Write your code above 👆
+print(result)
+
